316865e54d7bf38507eddea1d713964ed4f6c8ee
bestboontech <bestboontech@gmail.com>
2022-07-06 15:48:22 +0100

    fix

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -5,11 +5,12 @@
 
 /**
  * struct listint_s - singly linked list
+ *
  * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
+ *
  */
 typedef struct listint_s
 {
@@ -22,3 +23,4 @@ listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
+#endif /* LISTS_H */
